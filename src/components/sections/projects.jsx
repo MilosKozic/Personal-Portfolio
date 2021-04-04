5ee895e0ec4665ea2e9b90bd0a8200ebd13bc84f
MilosKozic <milos.kozic1986@gmail.com>
2021-04-04 22:07:47 +0200

    m

--- src/components/sections/projects.jsx
+++ src/components/sections/projects.jsx
@@ -4,7 +4,7 @@ const Projects =()=>{
             <h4 className="subtitle">Some Things I've Built |</h4>
         <ul className="projects">         
             <li>
-            <a target="_blank" rel="noreferrer" href="http://MilosKozic.github.io/COUNTRIES_APP">Countries.app()<span> // Website with data and forecast for any country in world</span></a>
+            <a target="_blank" rel="noreferrer" href="http://MilosKozic.github.io/COUNTRIES_APP">Countries.app()<span> // Website with data and forecast for any country in the world</span></a>
             </li>
         </ul>          
     </div>
